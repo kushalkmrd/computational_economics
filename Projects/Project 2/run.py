45810e35838ad4e211d9deef947b61bf59b77e2e
kushalkmrd <kushalkmrd@gmail.com>
2021-12-10 19:00:58 -0800

    update

--- Projects/Project 2/run.py
+++ Projects/Project 2/run.py
@@ -1,135 +1,228 @@
-# Computational Economics, Chapman University Fall 2021
-# Instructor: Dr. Ryan French
-##Project 2
-# Author: Kushal Kumar Reddy
-# kreddy@chapman.edu
-
-
+from numpy.random import rand
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import random
+from multiprocessing import Pool
+import time
+import gc
 
-class Agent:
-    """An Agent Class representing Each Agent, it has 3 parameters"""
-    def __init__(self, model):
-        self.model = model
-        self.is_alive = 0
-        self.x = 0
-        self.y = 0
+ele_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ  "
+target = "ALPHA CODE X LOAD TEST MAGIC K" * 3
+# target="ABCDE"
+tlen = len(target)
+tlen
+min_fitness_val = 10
 
-class Model:
-    """Model Class representing the Model layout to store the Grid, Agents and Update them each period"""
 
-    # Initialisation Function
+# print(dnac(25))
+
+def gfitness_cal(dna):
+    f = 0
+    for i in range(tlen):
+        if (target[i] == dna[i]):
+            f = f + 1
+    return f
+
+
+class gene_func:
+
+    def to_char(self, x):
+        return ele_list[x]
+
+    def dnac(self):
+        return ''.join(list(map(self.to_char, list(map(int, rand(tlen) * 27)))))
+
+
+class gene(gene_func):
+
+    # fitness calculation function
+    def fitness_cal(self):
+        f = 0
+        for i in range(tlen):
+            if (target[i] == self.dna[i]):
+                f = f + 1
+
+        return f
+
+    # manulaly create a DNA
+    def __init__(self, st=None, fit=None):
+        if st == None:
+            self.dna = self.dnac()
+        else:
+            self.dna = st
+        if fit == None:
+            self.fit = self.fitness_cal()
+        else:
+            self.fit = fit
+
+    # DNA mutation
+    def mutation(self):
+        pass
+
+    # function for crossover
+    def cross_over(self):
+        pass
+
+
+class Population:
     def __init__(self):
-        self.agents = {}  # Agents Dictionary
-        self.locations = {} # Locations of the grid
-        self.is_alive=[]  # Array to note the status of each Agent
+        self.arr = []
 
-    # Function to add agents and setup, with an option for Four Agents in Four Corners
-    def add_agents(self, n,p,is_4=0):
-        # Idea is to call each agent and loop them to place in grid one by one
-        self.agent_count=n
-        for i in range(n):
-            for j in range(n):
-                agent = Agent(self)
-                if is_4:
-                    if (i==n-1 and j==n-1) or (i==n-1 and j==0) or (i==0 and j==n-1) or (i==0 and j==0)  :
-                        agent.is_alive = 1
-                elif random.random() < p:
-                    agent.is_alive = 1
-                (agent.x, agent.y)=(i,j)
-                pos = (agent.x, agent.y)
-                self.agents[pos] = agent
-        self.is_alive = self.alive_agents()
+    # minimum required fitness
+    min_fit = min_fitness_val
 
-    # Function to obtain the neighbors of a cell, gets the reflected neighbors as well
-    def get_neighbors(self,pos):
-        #Set up the delta function and loop through it to get the neighbors, using a mod function
-        adj = {(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)}
-        li = []
-        x, y = pos
-        n = self.agent_count
-        for dy, dx in adj:
-                li.append(((x + dx)%n, (y + dy)%n))
-        alive=0
-        for posn in li:
-            if self.agents[posn].is_alive:
-                alive+=1
-        return alive
+    # max fitnss return
+    def cross_connect(self):
+        amt = int(len(self.arr) / 2)
+        i = 0
+        while i < amt:
+            try:
+                g = self.arr[i]
+                sendc(g.dna)
+                self.arr[i] = gene(recc())
+                i += 1
+            except:
+                print("error in sockets")
 
-    # Function to update agents each timestep
-    def update_agents(self):
-        #Get the agent status, and then use the rules to update them
-        current_agents = {}
-        agentscopy=self.agents.items()
-        alive_dict={}
-        for pos, agent in agentscopy:
-            alive_dict[pos]=self.get_neighbors(pos)
-        for pos, agent in agentscopy:
-            alive=alive_dict[pos]
-            if alive==3:
-                agent.is_alive=1
-            elif alive>3:
-                agent.is_alive=0
-            elif alive<2:
-                agent.is_alive=0
-            current_agents[pos]=agent
-        self.agents = current_agents
-        agents_alive=[]
-        for pos, agent in current_agents.items():
-            agents_alive.append(agent.is_alive)
-        self.is_alive =agents_alive
+    def max_fitness(self):
+        maxx = 0
+        for i in self.arr:
+            if i.fit > maxx:
+                maxx = i.fit
+        return maxx
 
-    def alive_agents(self):
-        agents_alive = []
-        for pos, agent in self.agents.items():            
-            agents_alive.append(agent.is_alive)
-        return agents_alive
+    # create a new population
+    def create_population(self, pop_size):
+        self.arr = []
+        i = 0
+        while i < pop_size:
+            v = gene()
+            if v.fit >= self.min_fit:
+                self.arr += [v]
+                i += 1
+                if (i % 5 == 0):
+                    print(i, " -- ", v.dna, "   ---   ", v.fit)
 
-if __name__ == "__main__":
-    # Function to return the array each time step for plotting
-    def runmodel_func(model,timesteps):
-        n = 50
-        model.add_agents(n, 0.1)
-        positions=[np.array(model.is_alive).reshape(n, n)]
-        for i in range(timesteps):
-            model.update_agents()
-            positions.append(np.array(model.is_alive).reshape(n, n))
-        return positions
+    def get_gene(self, pop, old_max):
+        size = len(pop.arr)
+        min_fitness = int(rand(1)[0] * old_max)
+        for i in range(1000):
+            selected_gene = int(rand(1)[0] * size)
+            # check if the dna quality matches the required mark or the max no of attempts have passed
+            if pop.arr[selected_gene].fit >= min_fitness or i > 200:
+                #                 if i>200:
+                #                     print("dna quality not found")
+                return pop.arr[selected_gene].dna
 
-    model = Model()
-    # To run 100 steps get the FPS and Seconds, for 100 frames
-    fps = 5
-    Seconds = 25
-    fig = plt.figure(figsize=(8, 8))
-    plt.title("Conway's Game of Life", fontsize=20)
-    snapshots = runmodel_func(model, fps * Seconds)
-    a = snapshots[0]
-    im = plt.imshow(a, interpolation='none', aspect='auto', vmin=0, vmax=1)
-    def animation_func(i):
-        im.set_array(snapshots[i])
-        return [im]
-    anim = animation.FuncAnimation(fig,animation_func,frames = Seconds * fps,blit=True)
-    writergif = animation.PillowWriter(fps=fps)
-    anim.save('game_of_life_50by50.gif',writer=writergif)
-    plt.show()
+    def cross_over_monte(self, old_pop, old_max, st, ed):
+        dna1 = self.get_gene(old_pop, old_max)
+        dna2 = self.get_gene(old_pop, old_max)
+        # val=int(rand(1)*(tlen-1))
+        val = int(rand(1)[0] * (ed - st) + st)
+        return ([dna1[:val] + dna2[val:], dna2[:val] + dna1[val:]])
+
+    # create a population from crossover
+    def cross_popx(self, varrs):
+        # print(pop_size)
+        sta = time.time()
+        pop_size, old_pop, max_old, ind, tot = varrs
+        # print("processor ",ind)
+        perp = len(old_pop.arr[0].dna) / tot
+        st = ind * perp
+        ed = st + perp
+        # arr=old_pop.arr
+        carr = []
+        i = 0
+        while i < pop_size:
+            vx = self.cross_over_monte(old_pop, max_old, st, ed)
+            v = gene(vx[0])
+            if v.fit >= self.min_fit:
+                carr += [v]
+                i += 1
+                # print(v.dna,"   ---   ",v.fit)
+            v = gene(vx[1])
+            if v.fit >= self.min_fit:
+                carr += [v]
+                i += 1
+                # print(v.dna,"   ---   ",v.fit)
+        if ind == 0:
+            print("\ntotal time=", (time.time() - sta))
+        # print("processor ",ind)
+        # print(carr)
+        # print(carr[0].dna)
+        return carr
+
+    def cross_pop(self, pop_size, old_pop, nop=4):
+        max_old = old_pop.max_fitness() + 1
+        jobs = []
+        # for k in range(nop):
+        #     p=mp(self.cross_popx(int(pop_size/nop),old_pop,max_old,k))
+        #     jobs.append(p)
+        #     p.start()
+        # for proc in jobs:
+        #     proc.join()
+        p = Pool(nop)
+
+        # workers = [p.apply_async(self.cross_popx, args=(int(pop_size/nop),old_pop,max_old,i,nop)) for i in range(nop)]
+
+        # for w in workers:
+        #	w.get()
+        values = p.map(self.cross_popx, [[int(pop_size / nop), old_pop, max_old, i, nop] for i in range(nop)])
+
+        # print(len(values[0]))
+        for i in values:
+            self.arr += i
+
+        for i in range(0, len(self.arr)):
+            # print(self.arr[i].dna,"  ---------- ",self.arr[i].fit)
+            if self.arr[i].dna == target:
+                print(self.arr[i].dna)
+                return (1)
+            # print(self.arr[i].)
+        # print(len(pro_dict))
+        return (0)
+        # mote carlo method of crossover rejection samaling
 
 
-# Uncomment In case to plot the figures in the Matplotlib without animation
-# if __name__ == "__main__":
-#     model = Model()
-#     n=30
-#     model.add_agents(n, 0.55)
-#     timesteps = 100
-#
-#     for i in range(timesteps):
-#         #model.add_agents(10,0.5)
-#         positions = np.array(model.is_alive).reshape(n,n)
-#         fig, ax = plt.subplots()
-#         img = ax.imshow(positions, interpolation='nearest')
-#         model.update_agents()
-#         plt.pause(1e-6)
-#     plt.show()
-# Get the funciton and run it
+def get_unique(p):
+    ar = []
+    for i in p.arr:
+        ar += [i.dna]
+
+    return (len(set(ar)) / len(ar))
+
+
+if __name__ == '__main__':
+
+    print("input population size")
+    psize = int(input())
+
+    cops = Population()
+    cops.create_population(psize)
+
+    while True:
+        print("enter the number of Population and no of processor to test with")
+        cr = int(input())
+        nop = int(input())
+        print("\n-----------------start-----------------\n")
+        cop = cops
+        st = time.time()
+        for i in range(cr):
+
+            print("----------crossover ", i, "---------")
+            copn = Population()
+            #    k=copn.cross_pop(psize,cop)
+            print(get_unique(cop))
+            # print("before-->",len(cop.arr))
+            k = copn.cross_pop(psize, cop, nop)
+            print(copn.max_fitness())
+            # print(get_unique(copn))
+            # print("after-->",len(cop.arr))
+            print("printing length of ", len(copn.arr))
+            if k == 1:
+                print("found")
+                break
+            del (cop)
+            cop = copn
+            gc.collect()
+
+        # print("\ntotal time=",(time.time()-st))
+        print("\n-----------------end-------------------\n\n")
